d9b96bc094e391abfd8a45010eab99580395c34f
MattHandzel <matthewwalkosz@gmail.com>
2022-11-09 22:07:32 -0600

    Added content to the about page, couldn't do that much styling

--- tom-peters-gitars-school-website/src/Pages/About.js
+++ tom-peters-gitars-school-website/src/Pages/About.js
@@ -1,10 +1,14 @@
 import "./About.css"
+import OLD_PERSON_IMAGE from "../images/oldMusicianStockImage.webp"
 
 export default function About(){
   return (
     <div className="aboutPage">
       <h1 className="aboutPage">About</h1>
-      
+      <img className="aboutPage" src={OLD_PERSON_IMAGE} alt=""/>
+      Tom began teaching guitar in 1973 at the highly acclaimed Chicago School of Music, where he studied under the guidance of Joe Rumoro Sr., one of the leading guitar players in the city.
+ 
+      Tom had an early start in musicoldMusicianStockImage.webpular guitar:  Blues, Jazz, Rock, Alternative, Metal, and Classical, concentrating on developing musical knowledge, reading skills, technique, music theory, and improvisation.  He tailors his lessons to fit each individual's wants & needs
     </div>
   )
 }
