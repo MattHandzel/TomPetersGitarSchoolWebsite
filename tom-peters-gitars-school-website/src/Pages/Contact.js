f7c248ac4ea9d743a6b2bf8751ac33fc907a3bcc
MattHandzel <matthewwalkosz@gmail.com>
2022-11-25 21:03:13 -0600

    i dont even know what i did, i think i added the map

--- tom-peters-gitars-school-website/src/Pages/Contact.js
+++ tom-peters-gitars-school-website/src/Pages/Contact.js
@@ -2,25 +2,23 @@ import Tile from "../Components/Tile";
 import img from "../images/background.jpg";
 import "./Contact.css";
 import { GmailIcon, HouseIcon, PhoneIcon } from "../Components/ContactIcons";
+import Gmap from "../images/gmap.png"
+
+import StoreFront from "../images/storefront.png"
 
 export default function Contact() {
   return (
     <>
-      <h1>Contact</h1>
-      <div className="contactPage">
-        <Tile
-          backgroundColor="white"
-          img={img}
-          text={
-            "We work with Chocolate Shoppe Ice Cream from Wisconsin to provide a premium cream experience in Illinois."
-          }
-          textAlign="right"
-        />
+        <img className="storefront" src={StoreFront} alt="storefront"/>
         <PhoneIcon/>
         <HouseIcon/>
         <GmailIcon/>
 
-      </div>
+        <a href="https://www.google.com/maps/place/Tom+Peters+Guitar+School/@41.6075137,-88.0489622,16.25z/data=!4m5!3m4!1s0x880e5d02161edbd9:0xc77c92cb94665c17!8m2!3d41.6076753!4d-88.0459558">
+          <img className="gmap" src={Gmap} alt="map"/>          
+        </a>
+
+        
     </>
   );
 }
